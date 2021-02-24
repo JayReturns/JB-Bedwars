84fd642f43f53632670849738d5447557bf47e65
Jay <jay3105@hotmail.com>
2021-02-24 16:43:37 +0100

    Added multi map support (sort of)

--- src/main/java/com/github/jayreturns/jbbedwars/map/LocationProvider.java
+++ src/main/java/com/github/jayreturns/jbbedwars/map/LocationProvider.java
@@ -9,18 +9,41 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class LocationProvider {
 
-    public Map<TeamColor, Location> getSpawnLocationMap(File file) throws FileNotFoundException {
+    /**
+     *
+     * @param file
+     * @return [Map1: SpawnLocations, Map2: BedLocations]
+     * @throws FileNotFoundException
+     */
+    public Map<TeamColor, Location>[] getLocationMaps(File file) throws FileNotFoundException {
         if (!file.exists()) {
             throw new FileNotFoundException("File " + file.getAbsolutePath() + " not found during spawn location scan");
         }
 
+        Map<TeamColor, Location> spawnLocations = new HashMap<>();
+        Map<TeamColor, Location> bedLocations = new HashMap<>();
+
+
         FileConfiguration config = convertFileToConfig(file);
 
-        return null;
+        for (String key : config.getConfigurationSection("teams").getKeys(false)) {
+            TeamColor color = TeamColor.valueOf(key.split(".")[0].toUpperCase());
+            if (!config.isLocation("teams." + key)) continue;
+            Location loc = config.getLocation("teams." + key);
+            if (key.contains("bed")) {
+                bedLocations.put(color, loc);
+            } else if (key.contains("spawn")) {
+                spawnLocations.put(color, loc);
+            }
+        }
+
+        Map<TeamColor, Location>[] maps = new Map[]{spawnLocations, bedLocations};
+        return maps;
     }
 
     private FileConfiguration convertFileToConfig(File file) {
