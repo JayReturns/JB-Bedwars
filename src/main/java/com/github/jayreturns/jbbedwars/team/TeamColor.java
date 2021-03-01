c3d394e4e12c72af8dfb401e60c9d16e5aafa060
Jay <jay3105@hotmail.com>
2021-03-01 23:50:52 +0100

    Refactored TeamColor.java

--- src/main/java/com/github/jayreturns/jbbedwars/team/TeamColor.java
+++ src/main/java/com/github/jayreturns/jbbedwars/team/TeamColor.java
@@ -1,30 +1,32 @@
 package com.github.jayreturns.jbbedwars.team;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.bukkit.Material;
 
-@AllArgsConstructor
-@Getter
 public enum TeamColor {
 
-    WHITE(Material.WHITE_WOOL),
-    ORANGE(Material.ORANGE_WOOL),
-    MAGENTA(Material.MAGENTA_WOOL),
-    LIGHT_BLUE(Material.LIGHT_BLUE_WOOL),
-    YELLOW(Material.YELLOW_WOOL),
-    LIME(Material.LIME_WOOL),
-    PINK(Material.PINK_WOOL),
-    GRAY(Material.GRAY_WOOL),
-    LIGHT_GRAY(Material.LIGHT_GRAY_WOOL),
-    CYAN(Material.CYAN_WOOL),
-    PURPLE(Material.PURPLE_WOOL),
-    BLUE(Material.BLUE_WOOL),
-    BROWN(Material.BROWN_WOOL),
-    GREEN(Material.GREEN_WOOL),
-    RED(Material.RED_WOOL),
-    BLACK(Material.BLACK_WOOL);
+    WHITE(),
+    ORANGE(),
+    MAGENTA(),
+    LIGHT_BLUE(),
+    YELLOW(),
+    LIME(),
+    PINK(),
+    GRAY(),
+    LIGHT_GRAY(),
+    CYAN(),
+    PURPLE(),
+    BLUE(),
+    BROWN(),
+    GREEN(),
+    RED(),
+    BLACK();
 
-    public Material material;
+    public Material getMaterial() {
+        return Material.valueOf(this + "_WOOL");
+    }
+
+    public Material getBedMaterial() {
+        return Material.valueOf(this + "_BED");
+    }
 
 }
